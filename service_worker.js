02e521d56d183c88a91b7e649e6852069fe933fd
Dan <danpopa1998@gmail.com>
2018-05-27 11:53:53 +0200

    add restaurants to objectStore

--- service_worker.js
+++ service_worker.js
@@ -7,48 +7,14 @@ if ('serviceWorker' in navigator) {
 self.addEventListener('fetch', event => {
 
   // Prevent the default, and handle the request ourselves.
-  event.respondWith(async function () {
+  event.respondWith(function () {
     const url = new URL(event.request.url);
 
     if (url.pathname === '/restaurants') {
-      console.log('use indexedDb')
-      const DBOpenRequest = indexedDB.open("RestaurantsDB", 1);
-      DBOpenRequest.onsuccess = event => {
-        const db = event.target.result;
-        
-          var transaction = db.transaction(["restaurants"], "readwrite").objectStore('restaurants');
-          // report on the success of the transaction completing, when everything is done
-          transaction.oncomplete = function (event) {
-            console.log('transaction complete', event.target);
-          };
-
-        transaction.onerror = function (event) {
-          console.log('transaction error')
-        };
-
-      }
-      return await fetch(event.request)
+      return useIndexedDb(event);
     } else {
-      //
-      // Try to get the response from a cache.
-      //
-      const cachedResponse = await caches.match(event.request);
-
-      //
-      // Return it if we found one.
-      //
-      if (cachedResponse) return cachedResponse;
-      //
-      // If we didn't find a match in the cache, use the network.
-      //
-      const cache = await caches.open('v1');
-      const fetchedResource = await fetch(event.request);
-      cache.put(event.request, fetchedResource.clone());
-
-      return fetchedResource
+      return useCache(event);
     }
-
-
   }());
 });
 
@@ -70,3 +36,54 @@ self.addEventListener('activate', event => {
     var objectStore = db.createObjectStore("restaurants", { keyPath: "id" });
   }
 })
+
+async function useIndexedDb(fetchEvent) {
+  console.log('use indexedDb')
+  const fetchedRestaurants = await fetch(fetchEvent.request);
+  const items = await fetchedRestaurants.json();
+
+  const DBOpenRequest = indexedDB.open("RestaurantsDB", 1);
+  DBOpenRequest.onsuccess = async event => {
+
+    const db = event.target.result;
+
+    //
+    // create an object store on the transaction
+    //
+
+    console.log(123)
+   
+    var objectStore = db.transaction('restaurants', 'readwrite').objectStore("restaurants");
+    items.map(restaurant => objectStore.put(restaurant))        
+
+    objectStore.onsucces = async function () {
+      console.log('object store success')
+    }
+
+    objectStore.onerror = function () {
+      console.log('err')
+    }
+
+  }
+  return await fetch(fetchEvent.request)
+}
+
+async function useCache(event) {
+  //
+  // Try to get the response from a cache.
+  //
+  const cachedResponse = await caches.match(event.request);
+
+  //
+  // Return it if we found one.
+  //
+  if (cachedResponse) return cachedResponse;
+  //
+  // If we didn't find a match in the cache, use the network.
+  //
+  const cache = await caches.open('v1');
+  const fetchedResource = await fetch(event.request);
+  cache.put(event.request, fetchedResource.clone());
+
+  return fetchedResource
+}
